b0dce61459472c1c794574118bdc535d0128110c
Gabrielletrr <gabrielletrr17@gmail.com>
2025-11-01 15:11:51 -0300

    .

--- Soma de 2 numeros.py
+++ Soma de 2 numeros.py
@@ -1,6 +1,6 @@
-n1 = float(input('Digite um numero: '))
-n2 = float(input('Digite um numero: '))
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
 
 soma = n1 + n2
 
-print('A soma dos numeros é:', soma)
+print('A soma entre os números {} e {} é: {}'.format(n1,n2,soma))
