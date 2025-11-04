474a7ffd3032527361d271888a21cbc25ba1a228
Gabrielletrr <gabrielletrr17@gmail.com>
2025-11-04 00:00:12 -0300

    .

---
+++ 009-Tabuada.py
@@ -0,0 +1,24 @@
+n = int(input('Digite um número inteiro: '))
+
+um = n*1
+dois = n*2
+tres = n*3
+quatro = n*4
+cinco= n*5
+seis = n*6
+sete = n*7
+oito = n*8
+nove = n*9
+dez = n*10
+
+print('----------Tabuada do {}----------'.format(n))
+print('1 x {} = {}'.format(n,um))
+print('2 x {} = {}'.format(n,dois))
+print('3 x {} = {}'.format(n,tres))
+print('4 x {} = {}'.format(n,quatro))
+print('5 x {} = {}'.format(n,cinco))
+print('6 x {} = {}'.format(n,seis))
+print('7 x {} = {}'.format(n,sete))
+print('8 x {} = {}'.format(n,oito))
+print('9 x {} = {}'.format(n,nove))
+print('10 x {} = {}'.format(n,dez))
